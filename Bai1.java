921e05413c0820bda2c4230024b0a912bccadd29
KKhang <127233909+SevKan10@users.noreply.github.com>
2023-03-20 06:01:31 +0700

    Add files via upload

---
+++ Bai1.java
@@ -0,0 +1,17 @@
+
+package Bai1;
+
+
+public class Bai1 
+{
+
+	public static void main(String[] args) 
+	{
+
+		System.out.println("Hello, World!\nKhuu Khang\n16 yrs old");
+		// \n để xuống dòng, nằm trong nháy
+		System.out.print("1+1 = "1+1);
+		//System.out.println(1+1  );
+	}
+
+}
