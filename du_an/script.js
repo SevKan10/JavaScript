95728433b8b283d9faead14f821cac045eabbaf1
KKhang <khangkhuu10727@gmail.com>
2023-06-09 11:31:17 +0700

    ..

---
+++ du_an/script.js
@@ -0,0 +1,22 @@
+h1 {
+    text-align: center;
+}
+
+#product-list {
+    display: flex;
+    justify-content: center;
+}
+
+.product {
+    margin: 20px;
+    text-align: center;
+}
+
+.product img {
+    width: 200px;
+    height: 200px;
+}
+
+#product-details {
+    text-align: center;
+}
